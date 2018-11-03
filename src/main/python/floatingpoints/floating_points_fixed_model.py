0b5cc5ea36391c84b15e60560526365dd87c11bc
Manuel Kisser <mkisser@student.tgm.ac.at>
2018-11-03 01:04:10 +0100

    Worker-done

--- src/main/python/floatingpoints/floating_points_fixed_model.py
+++ src/main/python/floatingpoints/floating_points_fixed_model.py
@@ -1,10 +1,12 @@
 import random
 from multiprocessing import Array
+import src.main.python.floatingpoints.floating_points_fixed_worker as worker
 class FloatingPointModel():
     def __init__(self, width, height):
         self.points = []
         self.width = width
         self.height = height
+        self.max_speed = 15
 
     def addPoint(self, color, radius):
         new_point = Array('i', 5)
@@ -13,9 +15,16 @@ class FloatingPointModel():
         new_point[2] = 1
         new_point[3] = color
         new_point[4] = radius
-        self.points.append(new_point)
-
+        point_worker = worker.WorkerProcess(new_point, self.max_speed, self.width, self.height)
+        point_worker.start()
+        self.points.append([new_point, point_worker])
         pass
 
     def removePoint(self):
+        point_worker = self.points.pop()
+        point_worker[0][2] = 0
+        point_worker[1].close()
+        point_worker[1] = None
+        point_worker[0] = None
+        point_worker = None
         pass
--- src/main/python/floatingpoints/floating_points_fixed_worker.py
+++ src/main/python/floatingpoints/floating_points_fixed_worker.py
@@ -3,12 +3,17 @@ from multiprocessing import Process
 import random
 
 class WorkerProcess(Process):
-    def __init__(self, point_info, width, height, max_speed):
+    """
+    Describes a worker Process that processes point coordinates while taking
+    window width, height and max speed into account
+    """
+    def __init__(self, point_info, max_speed, width, height):
         """
-
-        :param point_info:
-        :param width:
-        :param height:
+        Initiates the Process
+        :param point_info: 0:x pos, 1:y pos, 2:alive. 3:color, 4:radius
+        :param width: Window width
+        :param height: Window height
+        :param max_speed: Max point speed
         """
         Process.__init__(self)
         self.point_info = point_info
@@ -18,8 +23,8 @@ class WorkerProcess(Process):
 
     def run(self):
         """
-
-        :return:
+        Generates velocity according to max speed and moves point
+        :return: None
         """
         vx = random.randint(1, self.max_speed)
         vy = random.randint(1, self.max_speed)
